dc5898b50822ef70a7b5dfd1c68b3a7dc1bbb6e5
rossq <rossq@umich.edu>
2022-03-14 18:29:05 -0400

    Login form on homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,9 +1,35 @@
 import React from "react";
 
-import LineChart from "./components/LineChart";
+import { Button, Container, Form, Row } from "react-bootstrap";
 
 export default function Homepage() {
-  return <div>Dis da Homepage
-    <LineChart />
+  return <div>
+
+    <Container>
+      <Row>
+        Dis da Homepage
+      </Row>
+      <Row>
+        <Form className="rounded">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type="email" placeholder="Enter Email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="password" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember Me" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+        </Form>
+      </Row>
+    </Container>
   </div>;
 }
