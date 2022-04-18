057bee7889a8ddcf7f831d3f5f8296249a0da02f
Xiangyu Qin <qinx@umich.edu>
2022-04-18 19:28:24 -0400

    error msg

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { Button, Container, Card, Form, Row, Tabs, Tab } from "react-bootstrap";
+import {
+  Button,
+  Alert,
+  Container,
+  Card,
+  Form,
+  Row,
+  Tabs,
+  Tab,
+} from "react-bootstrap";
 import Nav from "./Nav";
 
 export default function Homepage() {
+  const [error, setError] = useState(0);
+
+  const attemptLogin = (link) => {
+    if (error === 2) window.location.href = link;
+    else {
+      setError(error + 1);
+    }
+  };
+
+  const errorMsg =
+    error === 1
+      ? "The Email Address field is required"
+      : "Invalid username or Password";
+
   return (
     <div
       style={{
@@ -28,21 +51,42 @@ export default function Homepage() {
             <Container>
               <Row>
                 <Form className="rounded">
+                  {error !== 0 && (
+                    <Alert
+                      variant="danger"
+                      style={{
+                        padding: "10px 0px 0px 10px",
+                      }}
+                    >
+                      <p>{errorMsg}</p>
+                    </Alert>
+                  )}
                   <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email Address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter Email" />
+                    <Form.Control
+                      type="email"
+                      placeholder="Enter Email"
+                      required
+                    />
                   </Form.Group>
 
                   <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="password" />
+                    <Form.Control
+                      type="password"
+                      placeholder="password"
+                      required
+                    />
                   </Form.Group>
 
                   <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Remember Me" />
                   </Form.Group>
 
-                  <Button variant="primary" type="submit" href="/athlete">
+                  <Button
+                    variant="primary"
+                    onClick={() => attemptLogin("/athlete")}
+                  >
                     Login
                   </Button>
                 </Form>
@@ -55,9 +99,23 @@ export default function Homepage() {
             <Container>
               <Row>
                 <Form className="rounded">
+                  {error !== 0 && (
+                    <Alert
+                      variant="danger"
+                      style={{
+                        padding: "10px 0px 0px 10px",
+                      }}
+                    >
+                      <p>{errorMsg}</p>
+                    </Alert>
+                  )}
                   <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email Address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter Email" />
+                    <Form.Control
+                      type="email"
+                      placeholder="Enter Email"
+                      required
+                    />
                   </Form.Group>
 
                   <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -66,10 +124,13 @@ export default function Homepage() {
                   </Form.Group>
 
                   <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Remember Me" />
+                    <Form.Check type="checkbox" label="Remember Me" required />
                   </Form.Group>
 
-                  <Button variant="primary" type="submit" href="/coach">
+                  <Button
+                    variant="primary"
+                    onClick={() => attemptLogin("/coach")}
+                  >
                     Login
                   </Button>
                 </Form>
